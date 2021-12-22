ce6568790ecd0a5abe68394de6cea57f53422523
Vedika Utekar <84115765+Vedika03@users.noreply.github.com>
2021-12-22 18:49:53 +0530

    Project Initialized

---
+++ ITApex.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITApex.Web.Startup))]
+namespace ITApex.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
